fa88a7b29e6fcf03fa754e4969705366a178ba3d
Remy D'Agostino <remy.dagostino@rexsoftware.com.au>
2014-03-29 11:49:44 +1000

    Added autoprefixer and a hand full of other great css files to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	browserify = require('gulp-browserify'),
-	less = require('gulp-less'),
-	clean = require('gulp-clean'),
-	uglify = require('gulp-uglify'),
-	serve = require('gulp-serve'),
-	jshint = require('gulp-jshint');
+  gutil = require('gulp-util'),
+  browserify = require('gulp-browserify'),
+  prefix = require('gulp-autoprefixer'),
+  less = require('gulp-less'),
+  clean = require('gulp-clean'),
+  uglify = require('gulp-uglify'),
+  serve = require('gulp-serve'),
+  jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
   gulp.src('./src/js/**/*.js')
@@ -15,47 +16,48 @@ gulp.task('lint', function() {
 
 // Cleanup
 gulp.task('clean', function() {
-	gulp.src('build/*', { read: false})
-		.pipe(clean());
+  gulp.src('build/*', { read: false})
+    .pipe(clean());
 });
 
 // Scripts
 gulp.task('scripts', function() {
-	gulp.src('src/js/main.js')
-		.pipe(browserify({
-			debug: true
-		}))
-		.on('error', gutil.log)
-		// .pipe(uglify({outSourceMap: true}))
-		.pipe(gulp.dest('./build/js'));
+  gulp.src('src/js/main.js')
+    .pipe(browserify({
+      debug: true
+    }))
+    .on('error', gutil.log)
+    // .pipe(uglify({outSourceMap: true}))
+    .pipe(gulp.dest('./build/js'));
 });
 
 // Stylesheets
 gulp.task('less', function(){
-	gulp.src('src/css/style.less')
-		.pipe(less({
-			paths: ['src/less']
-		}))
-		.on('error', gutil.log)
-		.pipe(gulp.dest('./build/css'));
+  gulp.src('src/css/style.less')
+    .pipe(less({
+      paths: ['src/less']
+    }))
+    .on('error', gutil.log)
+    .pipe(prefix())
+    .pipe(gulp.dest('./build/css'));
 });
 
 // Index, images, fonts
 gulp.task('assets', function(){
-	gulp.src('src/index.html')
-		.pipe(gulp.dest('./build/'));
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('./build/'));
 
-	gulp.src('src/img/**/*')
-		.pipe(gulp.dest('./build/img'));
+  gulp.src('src/img/**/*')
+    .pipe(gulp.dest('./build/img'));
 
-	gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('./build/fonts'));
+  gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('./build/fonts'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('./src/css/**/*', ['less']);
-	gulp.watch(['./src/js/**/*.js'], ['scripts', 'lint']);
-	gulp.watch(['./src/img/**/*', './src/fonts/**/*', './src/index.html'], ['assets']);
+  gulp.watch('./src/css/**/*', ['less']);
+  gulp.watch(['./src/js/**/*.js'], ['scripts', 'lint']);
+  gulp.watch(['./src/img/**/*', './src/fonts/**/*', './src/index.html'], ['assets']);
 });
 
 gulp.task('serve', serve('./build'));
